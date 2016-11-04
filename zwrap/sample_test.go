eba42a76ccb8121d5f6e875a3b6074f91ad15f64
Joshua T Corbin <joshua@uber.com>
2016-11-04 07:56:20 -0700

    TestSampleCheckPanicFatal: explicate "fatal always checks OK"

--- zwrap/sample_test.go
+++ zwrap/sample_test.go
@@ -180,7 +180,9 @@ func TestSamplerCheckPanicFatal(t *testing.T) {
 
 		assert.Nil(t, sampler.Check(zap.DebugLevel, "foo"), "Expected a nil CheckedMessage at disabled log levels.")
 		for i := 0; i < 5; i++ {
-			if cm := sampler.Check(level, "sample"); cm.OK() {
+			cm := sampler.Check(level, "sample")
+			assert.True(t, cm.OK(), "expected fatal level to always be OK")
+			if cm.OK() {
 				cm.Write(zap.Int("iter", i))
 			}
 		}
